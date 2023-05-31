512d34e5db48a4022336f03288ce58c027368170
Daniel <angelicodaniel93@gmail.com>
2023-05-31 15:35:04 +0200

    last commit

--- src/org/lessons/java/FizzBuzz.java
+++ src/org/lessons/java/FizzBuzz.java
@@ -11,9 +11,10 @@ public class FizzBuzz {
         System.out.println("Quanti numeri vuoi stampare? Inserisci un numero maggiore di 0 e minore di 1000: ");
         int printedNumbers = keyboardReader.nextInt();
 
-        if (printedNumbers < 0 || printedNumbers > 1000)
+        if (printedNumbers < 0 || printedNumbers > 1000) {
             System.out.println("Non hai inserito un numero maggiore di 0 e minore di 1000");
-
+            return;
+        }
         for (int i = 0; i <= printedNumbers ; i++) {
          if (i % 3 == 0 && i % 5 == 0){
              System.out.println("FizzBuzz ");
