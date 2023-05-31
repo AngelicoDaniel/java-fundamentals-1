6fad070d254857e99e25cf6bb72769d19b138cc8
Daniel <angelicodaniel93@gmail.com>
2023-05-31 15:08:52 +0200

    no magic numbers

--- src/org/lessons/java/FizzBuzz.java
+++ src/org/lessons/java/FizzBuzz.java
@@ -1,12 +1,22 @@
 package org.lessons.java;
 
+import java.util.Scanner;
+
 public class FizzBuzz {
     public static void main(String[] args) {
         //int[] fizzbuzz = new int[100];
+        Scanner keyboardReader = new Scanner(System.in);
 
-        for (int i = 0; i <= 100 ; i++) {
+
+        System.out.println("Quanti numeri vuoi stampare? Inserisci un numero maggiore di 0 e minore di 1000: ");
+        int printedNumbers = keyboardReader.nextInt();
+
+        if (printedNumbers < 0 || printedNumbers > 1000)
+            System.out.println("Non hai inserito un numero maggiore di 0 e minore di 1000");
+
+        for (int i = 0; i <= printedNumbers ; i++) {
          if (i % 3 == 0 && i % 5 == 0){
-             System.out.print("FizzBuzz ");
+             System.out.println("FizzBuzz ");
          } else if (i % 3 == 0) {
              System.out.println("Fizz ");
          } else if (i % 5 == 0) {
--- src/org/lessons/java/TrainTicketCalculation.java
+++ src/org/lessons/java/TrainTicketCalculation.java
@@ -5,20 +5,25 @@ import java.util.Scanner;
 
 public class TrainTicketCalculation {
     public static void main(String[] args) {
+
+        double pricePerKm = 0.21;
+        double discountUnder18 = 0.2;
+        double discountOver65 = 0.4;
+
         Scanner keyboardReader = new Scanner(System.in);
 
-        System.out.println("Quanti kilometri vuoi percorrere?");
+        System.out.println("Quanti chilometri vuoi percorrere?");
         double km = keyboardReader.nextDouble();
 
         System.out.println("Quanti anni hai?");
         int age = keyboardReader.nextInt();
 
-        double price = km * 0.21;
+        double price = km * pricePerKm;
 
         if (age <= 18) {
-            price -= price * 0.2;
+            price -= price * discountUnder18;
         } else if (age >= 65) {
-            price -= price * 0.4;
+            price -= price * discountOver65;
         }
 
         DecimalFormat decimalFormat = new DecimalFormat("#.##");
