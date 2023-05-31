c42608256feb178f5c1c28890b7ef2c97d3af54a
Daniel <angelicodaniel93@gmail.com>
2023-05-31 14:38:12 +0200

    fizzbuzz

--- src/org/lessons/java/FizzBuzz.java
+++ src/org/lessons/java/FizzBuzz.java
@@ -2,7 +2,7 @@ package org.lessons.java;
 
 public class FizzBuzz {
     public static void main(String[] args) {
-//        int[] fizzbuzz = new int[100];
+        //int[] fizzbuzz = new int[100];
 
         for (int i = 0; i <= 100 ; i++) {
          if (i % 3 == 0 && i % 5 == 0){
